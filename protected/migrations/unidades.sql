e70bbfb30fdf212810de942cee29444b7ef94f0a
kenny.capote <kenny.capote@gmail.com>
2014-09-27 00:06:15 +0000

    tabla unidades

---
+++ protected/migrations/unidades.sql
@@ -0,0 +1,59 @@
+CREATE TABLE public.GEN_UNIDADES 
+(
+  CUNIDAD VARCHAR(4 ) NOT NULL 
+, DUNIDAD VARCHAR(70) NOT NULL 
+, SESION_ID VARCHAR(30 ) DEFAULT 0 NOT NULL 
+, CONSTRAINT PK_GUN_SPKUNIDAD PRIMARY KEY 
+  (
+    CUNIDAD 
+  )
+  
+) ;
+
+
+
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('01','Bolsas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('02','Cajas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('03','Centímetros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('04','Centímetros Cuadrados','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('05','Centímetros Cúbicos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('06','Cuartos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('07','Galones','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('08','Gramos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('09','Gruesas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('10','Kilogramos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('11','Kilómetros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('12','Kilómetros Cuadrados','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('13','Libras','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('14','Litros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('15','Madejas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('16','Metros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('17','Metros Cuadrados','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('18','Metros Cúbicos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('19','Mililitros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('20','Milímetros','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('22','Onzas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('23','Paletas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('24','Panelas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('25','Pies','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('26','Pies Cuadrados','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('27','Pies Cúbicos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('28','Piezas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('29','Pulgadas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('30','Rollos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('31','Sacos','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('32','Toneladas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('33','Toneladas Métricas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('34','Toneladas Cúbicas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('35','Unidades','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('36','Yardas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('37','Docenas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('38','Pares','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('39','Miles de Semillas','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('40','Millar','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('41','Juegos de Kit','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('42','Tambores','0');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('43','Paquetes','AAACFGAAGAAAALiAAq');
+Insert into public.GEN_UNIDADES (CUNIDAD,DUNIDAD,SESION_ID) values ('44','Miles','0');
+-- No se ha podido presentar el DDL INDEX para el objeto PRODUCCION.PK_GUN_CUNIDAD mientras que DBMS_METADATA intenta utilizar el generador interno.
+
